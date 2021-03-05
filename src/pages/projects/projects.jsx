20f89849bb2ec1c894b47fe5da5dfba0b052c4a3
Cojocaru Ionut Liviu <cojocaruionutliviu@gmail.com>
2021-03-05 22:37:32 +0200

    added repo cards

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -6,22 +6,57 @@ export class Projects extends React.Component {
             <div id="container-projects">
                 <div className="repo-cards">
                      <a href="https://github.com/CojocaruionutLiviu/my-portfolio" target="_blank">
-                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/my-portfolio.svg' />
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/my-portfolio.svg?fullname=' />
                     </a>
                 </div>
                 <div className="repo-cards">
-                     <a href="https://github.com/CojocaruionutLiviu/my-portfolio" target="_blank">
-                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/my-portfolio.svg' />
+                     <a href="https://github.com/CojocaruionutLiviu/MyIMDB" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/MyIMDB.svg?fullname=' />
                     </a>
                 </div>
                 <div className="repo-cards">
-                     <a href="https://github.com/CojocaruionutLiviu/my-portfolio" target="_blank">
-                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/my-portfolio.svg' />
+                     <a href="https://github.com/CojocaruionutLiviu/Reactjs-rock-paper-scissor" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/Reactjs-rock-paper-scissor.svg?fullname=' />
                     </a>
                 </div>
                 <div className="repo-cards">
-                     <a href="https://github.com/CojocaruionutLiviu/my-portfolio" target="_blank">
-                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/my-portfolio.svg' />
+                     <a href="https://github.com/CojocaruionutLiviu/bootstrap-web-design" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/bootstrap-web-design.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/deck-of-cards" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/deck-of-cards.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/OOP-deck-of-cards" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/OOP-deck-of-cards.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/Ajax-NASA-pic-of-the-day" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/Ajax-NASA-pic-of-the-day.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/ajax-activity-picker" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/ajax-activity-picker.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/ajax-game-list" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/ajax-game-list.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/DOM-book-list" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/DOM-book-list.svg?fullname=' />
+                    </a>
+                </div>
+				<div className="repo-cards">
+                     <a href="https://github.com/CojocaruionutLiviu/DOM-calendar" target="_blank">
+                        <img className="img-card" src='https://gh-card.dev/repos/CojocaruionutLiviu/DOM-calendar.svg?fullname=' />
                     </a>
                 </div>
             </div>
