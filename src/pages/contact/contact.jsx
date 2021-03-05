6995e08ac1d03ec123f653e1acde635898a2db3e
Cojocaru Ionut Liviu <cojocaruionutliviu@gmail.com>
2021-03-05 02:05:46 +0200

    form valitation done

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -3,31 +3,71 @@ import sep from './../../images/sep.svg'
 import React from 'react'
 import emailjs from 'emailjs-com';
 
+const initialState= {
+	name: "",
+			reply: "",
+			phone: "",
+			message: "",
+			nameError: "",
+			replyError: "",
+			phoneError: "",
+			messageError: "",
+			submitted:"",
+}
+
 export class Contact extends React.Component {
 	constructor(props) {
 		super(props);
-		// this.state.inputs = {
-		// 	name: "",
-		// 	email: "",
-		// 	phone: "",
-		// 	message: "",
-		// 	nameError: "",
-		// 	emailError: "",
-		// 	phoneError: "",
-		// 	messageError: "",
+		this.state = initialState;
+	this.sendEmail = this.sendEmail.bind(this);
+}
+	validate = ()=>{
+			let nameError = "";
+			let replyError =  "";
+			let phoneError = "";
+			let messageError = "";
+
+			if(!this.state.name){
+				nameError="Cannot be empty"
+			}
+			if (!this.state.reply.includes('@')){
+				replyError = "Must contain @";
+			}
+			if(this.state.phone.length < 10){
+				phoneError="Must have 10 digits"
+			}
+			if(this.state.message.length < 1 ){
+				messageError= "Cannot be empty"
+			}
+			if(replyError||nameError||phoneError||messageError){
+				this.setState({replyError,nameError,phoneError,messageError});
+				return false;
+			}
+			return true;
+
 	}
-	// this.sendEmail = this.sendEmail.bind(this);
+
+	handleChange = event =>{
+		const isCheckbox = event.target.type ==='checkbox';
+		this.setState({
+			[event.target.name]: isCheckbox
+			? event.target.checked
+			: event.target.value
+		});
+	};
 
 	sendEmail = (e) => {
+		const isValid= this.validate();
 		e.preventDefault();
 
-		emailjs.sendForm('service_1roz64t', 'template_1tj75l7', e.target, 'user_zbRNu5XKe8aEuNGDUsNvp')
+		if (isValid){emailjs.sendForm('service_1roz64t', 'template_1tj75l7', e.target, 'user_zbRNu5XKe8aEuNGDUsNvp')
 			.then((result) => {
 				console.log(result.text);
 			}, (error) => {
 				console.log(error.text);
 			});
-		e.target.reset();
+			this.setState(initialState);
+		}
 	}
 
 	render() {
@@ -41,14 +81,40 @@ export class Contact extends React.Component {
 				<form className="contact-form" onSubmit={this.sendEmail}>
 					<input type="hidden" name="contact_number" />
 
-					<input type="text" name="from_name" placeholder="YOUR NAME" autocomplete="off" />
-
-					<input type="email" name="to_reply" placeholder="EMAIL" autocomplete="off" />
-
-					<input type="text" name="phone" placeholder="PHONE NUMBER" autocomplete="off" />
-
-					<textarea name="message" placeholder="LEAVE ME A MESSAGE" />
+					<input  
+					name="name" 
+					placeholder="YOUR NAME" 
+					autocomplete="off" 
+					value={this.state.name}
+					onChange={this.handleChange}
+					/>
+					<div style={{color:'red', fontSize:'12px'}}>{this.state.nameError}</div>
+					<input 
+					name="reply" 
+					placeholder="EMAIL" 
+					autocomplete="off" 
+					value={this.state.reply}
+					onChange={this.handleChange}
+					/>
+					<div style={{color:'red', fontSize:'12px'}}>{this.state.replyError}</div>
+					<input 
+					name="phone" 
+					placeholder="PHONE NUMBER" 
+					
+					value={this.state.phone}
+					onChange={this.handleChange}
+					/>
+					<div style={{color:'red', fontSize:'12px'}}>{this.state.phoneError}</div>
+					<textarea 
+					name="message" 
+					placeholder="LEAVE ME A MESSAGE" 
+					autocomplete="off" 
+					value={this.state.message}
+					onChange={this.handleChange}
+					/>
+					<div style={{color:'red', fontSize:'12px'}}>{this.state.messageError}</div>
 					<input type="submit" value="Send" className="btn" autocomplete="off" />
+					<div style={{color:'black', fontSize:'24px',fontWeight: 'bold'}}>{this.state.submitted}</div>
 				</form>
 			</div>
 		)
