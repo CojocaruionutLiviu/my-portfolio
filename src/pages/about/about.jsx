e91c1b97468f311436c7d3fd673a7988c649b24c
Cojocaru Ionut Liviu <cojocaruionutliviu@gmail.com>
2021-03-06 13:50:29 +0200

    m

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -15,9 +15,7 @@ export class About extends React.Component {
 					<div className='borderBox'>About Me</div>
 					<p>My Name is Liviu, I’m 27 years old, i was born in Vaslui, Romania and i grow up since when i was 6 in Parma, Italy where i lived untill my 20th.
 						I love coding, researching and finding problems where there is no solution. I love what I do and really makes me happy</p>
-					<Document 
-						file="./CV.pdf"
-					/>
+					<a className="btn" href="https://drive.google.com/file/d/1cAVlhaod23IsXlvz1u7jt3hRJ7UXNyNQ/view?usp=sharing"> TO MY RESUME</a>
 				</div>
 			</div>
 		)
